15db394776ba586616c28210e9b7019b136f4a95
Joey Yu <joey@itsjoeoui.com>
2021-09-29 22:09:43 -0400

    Coming soon...
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>&int;bogdad(dy)</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://cscloud.itsjoeoui.com/egHeryWQMwFZhEBSYX5U/download"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Coming soon...
         </a>
       </header>
     </div>
