8dd9d61f0116d98ac2f2934702037c2d43fb3b85
Joey Yu <joey@itsjoeoui.com>
2021-10-10 00:39:04 -0400

    Bogdaddy is on his way
    * Bring in fluentui
    
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { DefaultButton } from "@fluentui/react";
+import { Stack, IStackTokens } from "@fluentui/react/lib/Stack";
+import { ThemeProvider } from "@fluentui/react/lib/Theme";
+
+const geolocation: Geolocation = navigator.geolocation;
+
+const verticalGapStackTokens: IStackTokens = {
+  childrenGap: 10,
+  padding: 10,
+};
 
 function App() {
+  const [lat, setLat] = useState<number>();
+  const [lng, setLng] = useState<number>();
+  const [loc, setLoc] = useState<boolean>();
+
+  function getLocation() {
+    if (geolocation) {
+      geolocation.getCurrentPosition(findLocal, showError);
+    } else {
+      setLoc(false);
+    }
+    function findLocal(position: any) {
+      setLat(position.coords.latitude);
+      setLng(position.coords.longitude);
+      setLoc(true);
+    }
+    function showError(error: any) {
+      alert(error.message);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>&int;bogdad(dy)</p>
-        <a
-          className="App-link"
-          href="https://cscloud.itsjoeoui.com/egHeryWQMwFZhEBSYX5U/download"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Coming soon...
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <div className="App" onLoad={() => getLocation()}>
+        <header className="App-header">
+          <Stack tokens={verticalGapStackTokens}>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>&int;bogdad(dy)</p>
+            <a
+              className="App-link"
+              href="https://cscloud.itsjoeoui.com/egHeryWQMwFZhEBSYX5U/download"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Coming soon...
+            </a>
+            <DefaultButton onClick={() => getLocation()}>Suicide</DefaultButton>
+            <p>
+              {loc ? (
+                <p>
+                  Bogdaddy is on his way
+                  <br /> Latitude: {lat}
+                  <br /> Longitude: {lng}
+                </p>
+              ) : (
+                <p>Location is unavailable</p>
+              )}
+            </p>
+          </Stack>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
